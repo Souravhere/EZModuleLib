fa44182474ee690da0dbdb5add3da507b1f91721
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-08-17 21:31:31 +0530

    Few updates becauer the npm is not working

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,23 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
   build: {
     lib: {
-      entry: 'src/main.js',
-      name: 'EZModule',
-      formats: ['es', 'umd'],
-      fileName: (format) => `ezmodule.${format}.js`
+      entry: 'src/index.js', // Entry file for your library
+      name: 'EZModules', // Global variable name for UMD build
+      formats: ['es', 'umd'], // Build both ES module and UMD
+      fileName: (format) => `ezmodules.${format}.js`,
     },
     rollupOptions: {
-      external: ['react', 'react-dom', 'prop-types'],
+      // Ensure to externalize deps that shouldn't be bundled
+      external: ['react', 'react-dom'],
       output: {
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
-          'prop-types': 'PropTypes'
-        }
-      }
-    }
-  }
+        },
+      },
+    },
+  },
+  plugins: [react()],
 });
