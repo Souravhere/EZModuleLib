0f4f6dfc809fde632cc0f51cc97c191aee5a41a7
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-08-18 17:16:34 +0530

    Added BreadcrumbTrail component with theme and customization options

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 // src/Components/index.js
-export {default as Tooltip} from './ToolTip/Tooltip.jsx'
+export {default as Tooltip} from './ToolTip/Tooltip.jsx';
+export {default as BreadcrumbTrail} from './BreadcrumbTrail/BreadcrumbTrail.js'
 // Add more components exports here if needed
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 // src/main.js
 export { default as Tooltip } from './components/ToolTip/Tooltip';
+export { default as BreadcrumbTrail } from './components/BreadcrumbTrail/BreadcrumbTrail';
 // Add any additional exports here
