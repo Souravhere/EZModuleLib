31a9c28ca6054b0152039a87c2b2f8dc0133754f
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-08-26 22:43:44 +0530

    Library build and local system checked

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import  Tooltip  from './components/ToolTip/Tooltip';
+import  Tabs  from './components/Tabs/Tabs';
 
 
 function App() {
+  const tabData = [
+    { label: 'Tab 1', content: <div>Content for Tab 1</div> },
+    { label: 'Tab 2', content: <div>Content for Tab 2</div> },
+    { label: 'Tab 3', content: <div>Content for Tab 3</div> }
+  ];
   return (
+    <>
+    <Tabs 
+        tabs={tabData} 
+        theme="custom"
+        buttonStyles={{ backgroundColor: 'lightblue', color: 'white' }}
+        activeButtonStyles={{ backgroundColor: 'darkblue', color: 'white' }}
+        containerStyles={{ borderColor: 'lightgray' }}
+      />
     <div style={{ padding: '50px' }}>
       <Tooltip
         content="Hello, I am a tooltip!"
@@ -14,6 +29,7 @@ function App() {
         <button>Hover over me</button>
       </Tooltip>
     </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import Tooltip from './components/ToolTip/Tooltip';
 export { Tooltip };
+
 import BreadcrumbTrail from './components/BreadcrumbTrail/BreadcrumbTrail';
-export {BreadcrumbTrail};
-import { Tabs } from './components/Tabs/Tabs';
-export {Tabs};
+export { BreadcrumbTrail };
+
+import Tabs from './components/Tabs/Tabs';  // Correct the import for Tabs
+export { Tabs };
